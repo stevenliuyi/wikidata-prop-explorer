e4829c362ff3f0eda93c750f2f2885e0efd3498d
stevenliuyi <stevenliuyi@gmail.com>
2018-04-08 04:50:32 -0400

    add tree filter

--- src/components/PropTree.js
+++ src/components/PropTree.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 import { Treebeard } from 'react-treebeard'
+import { FormControl } from 'react-bootstrap'
 import treeStyle from '../utils/tree'
+import * as filters from '../utils/filter'
 
 class PropTree extends Component {
-  state = {}
+  state = {
+    filterText: '',
+    tree: {}
+  }
 
   componentWillMount() {
     this.onToggle = this.onToggle.bind(this)
+    this.onFilterChange = this.onFilterChange.bind(this)
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (Object.keys(this.state.tree).length === 0)
+      this.setState({ tree: nextProps.tree })
   }
 
   onToggle(node, toggled) {
@@ -25,13 +36,32 @@ class PropTree extends Component {
     this.props.onChange(node.qid)
   }
 
+  onFilterChange(e) {
+    const filterText = e.target.value
+    if (!filterText) return this.setState({ tree: this.props.tree, filterText })
+
+    let filtered = filters.filterTree(this.props.tree, filterText)
+    filtered = filters.expandFilteredNodes(filtered, filterText)
+    this.setState({ tree: filtered, filterText })
+  }
+
   render() {
     return (
-      <Treebeard
-        data={this.props.tree}
-        onToggle={this.onToggle}
-        style={treeStyle}
-      />
+      <div>
+        <div style={{ marginBottom: '5px' }}>
+          <FormControl
+            type="text"
+            value={this.state.filterText}
+            placeholder="Search the tree..."
+            onChange={this.onFilterChange}
+          />
+        </div>
+        <Treebeard
+          data={this.state.tree}
+          onToggle={this.onToggle}
+          style={treeStyle}
+        />
+      </div>
     )
   }
 }
