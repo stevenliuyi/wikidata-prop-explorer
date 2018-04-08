209ad0132ca93e3f02b4cc53f53dd40635e16f63
stevenliuyi <stevenliuyi@gmail.com>
2018-04-08 00:15:15 -0400

    add travis

---
+++ travis/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+ssh -tt -i ~/.ssh/id_rsa stevenliuyi@login.tools.wmflabs.org << EOF
+  become prop-explorer
+  webservice stop
+  rm -rf ./wikidata-prop-explorer
+  rm -rf ./public_html
+  mkdir ./public_html
+  git clone -b wmflabs https://$GITHUB_TOKEN@github.com/stevenliuyi/wikidata-prop-explorer.git
+  mv ./wikidata-prop-explorer/* ./public_html
+  rm -rf ./wikidata-explorer
+  webservice start
+  exit
+  exit
+EOF
