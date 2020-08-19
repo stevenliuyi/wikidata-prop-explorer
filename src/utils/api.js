19214d9191c568d1bb6c84b745547a3ea6b27fc6
stevenliuyi <stevenliuyi@gmail.com>
2020-08-18 20:08:38 -0400

    update service name for SSH

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,20 @@
 // fetch SPARQL results from Wikidata
-export const fetchSPARQLResult = sparql =>
-  fetch(`https://query.wikidata.org/sparql?${sparql}&format=json`)
-    .then(res => {
-      return res.status >= 400 ? null : res.json()
-    })
-    .catch(err => {
-      console.log(err)
-      return null
-    })
+export const fetchSPARQLResult = (sparql) =>
+    fetch(`https://query.wikidata.org/sparql?${sparql}&format=json`)
+        .then((res) => {
+            return res.status >= 400 ? null : res.json()
+        })
+        .catch((err) => {
+            console.log(err)
+            return null
+        })
 
 // fetch translations
-export const fetchToolTranslations = lang =>
-  fetch(
-    process.env.PUBLIC_URL === '/prop-explorer' // on wmflabs
-      ? `https://tools.wmflabs.org/tooltranslate/data/prop-explorer/${lang}.json`
-      : `https://cors-anywhere.herokuapp.com/https://tools.wmflabs.org/tooltranslate/data/prop-explorer/${lang}.json`
-  ).then(res => {
-    return res.status >= 400 ? {} : res.json()
-  })
+export const fetchToolTranslations = (lang) =>
+    fetch(
+        process.env.PUBLIC_URL === '/prop-explorer' // on wmflabs
+            ? `https://tooltranslate.toolforge.org/data/prop-explorer/${lang}.json`
+            : `https://cors-anywhere.herokuapp.com/https://tooltranslate.toolforge.org/data/prop-explorer/${lang}.json`
+    ).then((res) => {
+        return res.status >= 400 ? {} : res.json()
+    })
